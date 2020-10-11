9034731e6ad68f0cf9827cd68c9eacb1097e5525
ari1idont <arindam01012001@gmail.com>
2020-10-11 11:23:42 +0530

    Updates

---
+++ precache-manifest.c54f437feeec08c1f4e28670a161c23e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb71931d7c062329609f48e155fd9cb4",
+    "url": "/VIT_HACK/index.html"
+  },
+  {
+    "revision": "83cb637261601e47ad9c",
+    "url": "/VIT_HACK/static/css/main.37653b51.chunk.css"
+  },
+  {
+    "revision": "b67419ba9a08774b433f",
+    "url": "/VIT_HACK/static/js/2.938c338a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/VIT_HACK/static/js/2.938c338a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83cb637261601e47ad9c",
+    "url": "/VIT_HACK/static/js/main.b740d7f9.chunk.js"
+  },
+  {
+    "revision": "fc78138750df3a52a8ee",
+    "url": "/VIT_HACK/static/js/runtime-main.5db877ec.js"
+  },
+  {
+    "revision": "c1fe2fb166043744a492acd7cf35161d",
+    "url": "/VIT_HACK/static/media/protect.c1fe2fb1.jpg"
+  },
+  {
+    "revision": "4e37e0c76512f833580fd06ebabbae60",
+    "url": "/VIT_HACK/static/media/symptoms.4e37e0c7.jpg"
+  }
+]);
